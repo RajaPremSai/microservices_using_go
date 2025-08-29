5ee49a41e97a7baa660db1820780113c18f33ed8
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-08-29 23:53:59 +0530

    account

--- account/service.go
+++ account/service.go
@@ -1,6 +1,8 @@
 package account
 
-import "context"
+import (
+	"context"
+)
 
 type Service interface{
 	PostAccount (ctx context.Context,name string )(*Account,error)
@@ -24,13 +26,25 @@ func newService(r Repository)Service{
 }
 
 func (s *accountService)PostAccount(ctx context.Context,name string)(*Account,error){
-
+	a := &Account{
+		Name: name,
+		ID:ksuid.New().String(),
+	}
+	if err := s.repository.PutAccount(ctx,*a);err!=nil{
+		return nil,err
+	}
+	return a,nil
 }
 
 func (s *accountService)GetAccount(ctx context.Context,id string)(*Account,error){
-
+	return s.repository.GetAccountByID(ctx,id)
 }
 
 func (s *accountService) GetAccounts(ctx context.Context,skip uint64,take uint64)([]Account,error){
+	if take>100 || (skip ==0 && take==0){
+		take = 100
+	}
+
 	
+	return s.repository.ListAccounts(ctx,skip,take)
 }
